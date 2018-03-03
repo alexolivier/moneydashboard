ef9265718444486109975ebfc64615116d74c7ea
Alex Olivier <alex@alexolivier.me>
2018-03-03 13:28:33 +0000

    accounts table

--- cli.js
+++ cli.js
@@ -1,6 +1,10 @@
 
 const MoneyDashboard = require('./lib/MoneyDashboard')
 const program = require('commander')
+const Table = require('cli-table')
+const moment = require('moment')
+const accounting = require('accounting')
+const colors = require('colors')
 
 program
   .version('0.1.0')
@@ -11,7 +15,25 @@ program
   .command('accounts')
   .action(async (cmd) => {
     await MoneyDashboard.login(program.email, program.password)
-    console.log(await MoneyDashboard.accounts())
+    const accounts = await MoneyDashboard.accounts()
+    var table = new Table({
+      head: ['Name', 'Balance', 'Source', 'Last Refreshed']
+    })
+    let total = 0
+    accounts.map(account => {
+      total += account.Balance
+      const color = account.Balance >= 0 ? 'green' : 'red'
+      table.push([
+        account.Name,
+        colors[color](accounting.formatMoney(account.Balance, '£ ', 0)),
+        account.Institution.Name,
+        moment(account.LastRefreshed).fromNow()
+      ])
+    })
+    const color = total >= 0 ? 'green' : 'red'
+    table.push([colors.red('TOTAL'), colors[color](accounting.formatMoney(total, '£ ', 0)), ''])
+    console.log(table.toString())
+    // console.log(accounts)
   })
 
 program
